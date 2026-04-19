447fd1b6a0f38cf5f4c4eeb9fd96b93331f61db0
Robert F. Allen <rallen@uwlax.edu>
2026-04-19 18:39:43 -0500

    added ability to read from URLs

--- R/import.R
+++ R/import.R
@@ -1,4 +1,21 @@
-#' read_UWL_SR_DAC_SUBJECT_IR_ALL_reports
+#' read_UWL_SR_DAC_SUBJECT_IR_ALL_reports_URL
+#'
+#' @param manifest_file URL of manifest file
+#'
+#' @returns data.frame of all files read in manifest_file
+#' @export
+#'
+read_UWL_SR_DAC_SUBJECT_IR_ALL_reports_URL <- function(manifest_file) {
+  readr::read_csv(manifest_file) %>%
+    filter(!is.na(URL)) %>%
+    select(URL, date) %>%
+    purrr::map_dfr(~ {
+      readr::read_csv(.x, guess_max = 10000) %>%
+        dplyr::mutate(`Enrl Date` = as.Date(.y))
+    })
+}
+
+#' read_UWL_SR_DAC_SUBJECT_IR_ALL_reports_locally
 #'
 #' @param path
 #' @param file_pattern
@@ -6,7 +23,7 @@
 #' @returns data.frame of all files read in path of file_pattern format
 #' @export
 #'
-read_UWL_SR_DAC_SUBJECT_IR_ALL_reports <- function(path, file_pattern) {
+read_UWL_SR_DAC_SUBJECT_IR_ALL_reports_locally <- function(path, file_pattern) {
   list.files(
     path = path,
     pattern = paste0("^", file_pattern, "_.*\\.csv$"),
@@ -18,7 +35,18 @@ read_UWL_SR_DAC_SUBJECT_IR_ALL_reports <- function(path, file_pattern) {
       date <- stringr::str_extract(file_name, "\\d{4}-\\d{2}-\\d{2}")
       readr::read_csv(.x, guess_max = 10000) %>%
         dplyr::mutate(`Enrl Date` = as.Date(date))
-    }) %>%
+    })
+}
+
+#' clean_enrollment_data
+#'
+#' @param enrollment_data data.frame of raw enrollment data
+#'
+#' @returns data.frame of imported, and cleaned, enrollment data
+#' @export
+#'
+clean_enrollment_data <- function(raw_enrollment_data) {
+  raw_enrollment_data %>%
     format_columns %>%
     dplyr::rename(
       catalog.num = `catalog.#`,
@@ -33,7 +61,13 @@ read_UWL_SR_DAC_SUBJECT_IR_ALL_reports <- function(path, file_pattern) {
       )
     ) %>%
     tidyr::unite("inst.name", instr.f.name, inst.l.name, sep = " ", na.rm = TRUE) %>%
-    dplyr::relocate(inst.name, .before = class.stat)
+    dplyr::relocate(inst.name, .before = class.stat) %>%
+    remove_excluded_programs_and_components %>%
+    combine_duplicated_sections %>%
+    remove_associated_labs %>%
+    combine_slash_courses %>%
+    determine_minimum_enrollment %>%
+    add_department_info
 }
 
 #' read_enrollment_data
@@ -45,11 +79,19 @@ read_UWL_SR_DAC_SUBJECT_IR_ALL_reports <- function(path, file_pattern) {
 #' @export
 #'
 read_enrollment_data <- function(path, file_pattern) {
-  df <- read_UWL_SR_DAC_SUBJECT_IR_ALL_reports(path, file_pattern) %>%
-    remove_excluded_programs_and_components %>%
-    combine_duplicated_sections %>%
-    remove_associated_labs %>%
-    combine_slash_courses %>%
-    determine_minimum_enrollment %>%
-    add_department_info
+  read_UWL_SR_DAC_SUBJECT_IR_ALL_reports_locally(path, file_pattern) %>%
+    clean_enrollment_data
+}
+
+#' read_enrollment_data_from_web
+#'
+#' @param manifest_file URL
+#'
+#' @returns data.frame of imported, and cleaned, enrollment data
+#' @export
+#'
+read_enrollment_data_from_web <- function(manifest_file) {
+  read_UWL_SR_DAC_SUBJECT_IR_ALL_reports_URL(manifest_file) %>%
+    clean_enrollment_data
+  }
 }
