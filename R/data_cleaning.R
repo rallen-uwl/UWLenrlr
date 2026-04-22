c8dea734498ccc1d331f6537253d3654af42d78b
rallen-uwl <rallen@uwlax.edu>
2026-04-22 12:48:02 -0500

    fixed issue with column names from feather input

--- R/data_cleaning.R
+++ R/data_cleaning.R
@@ -11,6 +11,38 @@ format_columns <- function(df) {
     dplyr::select_all(tolower)
 }
 
+#' format_csv_columns
+#'
+#' @param df
+#'
+#' @export
+#'
+format_csv_columns <- function(df) {
+  df %>%
+    format_columns %>%
+    dplyr::rename(
+      catalog.num = `catalog.#`,
+      course.topic.1 = course.topic.8,
+      course.topic.2 = course.topic.9
+    )
+}
+
+#' format_feather_columns
+#'
+#' @param df
+#'
+#' @export
+#'
+format_csv_columns <- function(df) {
+  df %>%
+    format_columns %>%
+    dplyr::rename(
+      catalog.num = `catalog.#`,
+      course.topic.1 = course.topic,
+      course.topic.2 = course.topic.
+    )
+}
+
 #' add_department_info
 #'
 #' @param df
